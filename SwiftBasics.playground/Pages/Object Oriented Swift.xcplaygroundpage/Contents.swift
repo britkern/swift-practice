1b151907ccb7312a6edc2d7015767c7da8ef5d52
Brit <britain.kern@gmail.com>
2018-06-11 09:47:10 -0700

    Object Oriented

---
+++ SwiftBasics.playground/Pages/Object Oriented Swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+// Object Oriented Swift
+
+let x1 = 0
+let y1 = 0
+
+let coordinate1: (x: Int, y: Int) = (0, 0)
+coordinate1.x
+
+
+struct Point {
+    let x: Int  // Stored Property
+    let y: Int  // Stored Property
+    
+    
+    /// Returns the surrounding points in range
+    func points(inRange range: Int) {
+        
+    }
+}
+
+let coordinatePoint = Point(x: 0, y: 0)  // Instance
+
+coordinatePoint.x
+
+
