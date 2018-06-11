28e9588b74a5b784af329c35940bb5830aad23d3
Brit <britain.kern@gmail.com>
2018-06-11 10:13:25 -0700

    test

--- SwiftBasics.playground/Pages/Object Oriented Swift.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Object Oriented Swift.xcplaygroundpage/Contents.swift
@@ -12,15 +12,32 @@ struct Point {
     let y: Int  // Stored Property
     
     
-    /// Returns the surrounding points in range
-    func points(inRange range: Int) {
-        
+    /// Returns the surrounding points in range of
+    /// the current one
+    func points(inRange range: Int = 1) -> [Point] {
+        var results = [Point] ()
+
+        let lowerBoundOfXRange = x - range
+        let upperBoundOfXRange = x + range
+
+        let lowerBoundOfYRange = y - range
+        let upperBoundOfYRange = y + range
+
+        for xCoordinate in
+    
+
+
+
+        return results
+
     }
 }
 
 let coordinatePoint = Point(x: 0, y: 0)  // Instance
 
 coordinatePoint.x
+
+
 
 
 
