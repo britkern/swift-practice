19c0b70422e5efcf333b1f6d0477e28c13a4bd1e
Brit <britain.kern@gmail.com>
2018-06-12 13:23:55 -0700

    afternoon

--- SwiftBasics.playground/Pages/ValueVsReference.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/ValueVsReference.xcplaygroundpage/Contents.swift
@@ -4,9 +4,25 @@ struct User {
     var age: Int
 }
 
-var someUser = User(fullName: , email)
+var someUser = User(fullName: "Pasan Premaratne", email: "pasan@teamtreehouse.com", age: 28)
 
+var anotherUser = someUser
 
+someUser.email = "pasan.premaratne@teamtreehouse.com"
 
+class Person {
+    var fullName: String
+    var email: String
+    var age = Int
+    
+    init(name: String, email: String, age: Int) {
+        self.fullName = name
+        self.email = email
+        self.age = age
+    }
+}
 
+var somePerson = Person(name: "Tim Cook", email: "time.cook@apple.com", age: 54)
+var anotherPerson = somePerson
+somePerson.email = "tcook@apple.com"
 
