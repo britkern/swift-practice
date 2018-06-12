dab8a1879ae64dedcc8a85ac9711f24b61e37e82
Brit <britain.kern@gmail.com>
2018-06-12 10:23:22 -0700

    Overriding Methods
    laser tower

--- SwiftBasics.playground/Pages/Object Oriented Swift.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Object Oriented Swift.xcplaygroundpage/Contents.swift
@@ -39,7 +39,6 @@ struct Point {
 }
 
 // let coordinatePoint = Point(x: 0, y: 0)  // Instance
-
 // coordinatePoint.x
 // coordinatePoint.points()
 
@@ -60,8 +59,13 @@ class Enemy {
 }
 
 class SuperEnemy: Enemy {
- 
     
+    let isSuper: Bool = true
+    
+    override init(x: Int, y: Int) {
+        super.init(x: x, y: y)
+        self.life = 50
+    }
 }
 
 
@@ -99,12 +103,30 @@ class Tower {
     }
 }
 
+class LaserTower: Tower {
+    override init(x: Int, y: Int) {
+        super.init(x: x, y: y)
+        self.range = 100
+        self.strength = 100
+    }
+    override func fire(at enemy: Enemy) {
+        while enemy.life >= 0 {
+            enemy.decreasedLife(by: strength)
+        }
+        print("Enemy destroyed!")
+    }
+}
+
 
 let tower = Tower(x: 0, y: 0)
 let enemy = Enemy(x: 1, y: 1)
-let superEnemy = SuperEnemy(x: 0, y: 0)
+let superEnemy = SuperEnemy(x: 1, y: 1)
+let laserTower = LaserTower(x: 2, y: 2)
 
 
 tower.fire(at: enemy)
+tower.fire(at: superEnemy)
+laserTower.fire(at: enemy)
+laserTower.fire(at: superEnemy)
 
 
